1bc42c24dee27dd8ca41f79ad45529286dd85ea2
seungwoo <jsu7375@gmail.com>
2024-03-26 02:10:29 +0900

    feat: tsconfig, eslint 설정

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("hi")
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.listen(3030, () => {
+  console.log('Hello, typeep!');
+});
